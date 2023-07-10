cea6a1728e78ab3604ef59eb59d0eeff269dc923
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-07-10 16:39:13 -0500

    update

--- 01 download historic climate.R
+++ 01 download historic climate.R
@@ -7,4 +7,14 @@ install.packages('pacman')
 library(pacman)
 pacman::p_load(terra, fs, sf, tidyverse, glue, gtools, RColorBrewer)
 
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+bsin <- terra::vect('shp/Cuenca/Cuenca_Río_Apulo.shp')
+dpto <- terra::vect('shp/Base/dptos.gpkg')
+
+plot(bsin)
+plot(dpto)
 

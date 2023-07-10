4a2fbc1e73409e919786911a836fe817a135a51c
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-07-10 16:47:00 -0500

    update

--- 01 download historic climate.R
+++ 01 download historic climate.R
@@ -23,3 +23,23 @@ cund <- dpto[dpto$DPTO_CNMBR == 'CUNDINAMARCA',]
 # A simple plot
 plot(cund)
 plot(bsin, add = T, col = 'red')
+
+# Parameters to download --------------------------------------------------
+vars <- c('pr', 'tasmax', 'tasmin')
+mdls <- c('ACCESS-CM2', 'ACCESS-ESM1-5', 'BCC-CSM2-MR', 'CanESM5', 'CESM2-WACCM', 'CESM2', 'CMCC-CM2-SR5', 'CMCC-ESM2', 'CNRM-ESM2', 'CNRM-ESM2', 'CNRM-CM6-1', 'CNRM-ESM2-1', 'EC-Earth3-Veg-LR')
+
+# Functions ---------------------------------------------------------------
+down <- function(var, mdl, ab1, ab2){
+
+  # Proof
+  var <- 'tasmin'
+  mdl <- 'ACCESS-CM2'
+  ab1 <- 'r1i1p1f1'
+  ab2 <- 'gn'
+  
+}
+
+
+
+
+

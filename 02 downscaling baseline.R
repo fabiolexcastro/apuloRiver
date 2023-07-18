787b56cd35b6f6b989c623f946e8b1ca3246819a
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-07-18 16:14:03 -0500

    update

---
+++ 02 downscaling baseline.R
@@ -0,0 +1,24 @@
+
+
+## Downscaling historic climate
+## Jul 18th 2023
+
+# Load libraries ----------------------------------------------------------
+library(pacman)
+pacman::p_load(elevatr, terra, fs, sf, tidyverse, glue, gtools, RColorBrewer)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+bsin <- terra::vect('shp/Cuenca/Cuenca_Río_Apulo.shp')
+dpto <- terra::vect('shp/Base/dptos.gpkg')
+cund <- dpto[dpto$DPTO_CNMBR == 'CUNDINAMARCA',]
+
+# Download SRTM - Elevatr library -----------------------------------------
+
+
+
+
+

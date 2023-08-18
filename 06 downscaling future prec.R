26ce296943a756af77159888b168940d21b70460
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-08-18 15:33:26 -0500

    update

--- 06 downscaling future prec.R
+++ 06 downscaling future prec.R
@@ -1,4 +1,3 @@
-
 ## Downscaling future for precipitation variable
 ## Aug 11th 2023
 
@@ -58,13 +57,105 @@ down.ftre <- function(dir){
   fls.ftr
   
   # To tidy the years
-  yrs.hst <- basename(fls.hst) %>% 
-    str_split(., pattern = '_') %>% 
-    map_chr(., 8) %>% 
-    gsub('.nc$', '', .) %>% 
-    unique()
+  prd <- tibble(hist = c(1974:2014, 1974:2014, 2010:2014), 
+                hist_raw = c(1974:2014, 1974:2014, 2010:2014),
+                ftre = c(2015:(2015+40), 2055:2095, 2096:2100))
   
-  yrs.ftr <- basename(fls.ftr)
+  yrs.hst <- prd$hist
+  yrs.hst.raw <- prd$hist_raw
+  yrs.ftr <- prd$ftre
+  
+  # To read as a raster file into a map function
+  map(.x = 1:nrow(prd), .f = function(i){
+    
+    d <- 1 # Correr y borrar
+    
+    cat('To process: ', d, '\n')
+    pr <- prd[d,]
+    yr.bs <- as.numeric(pr[,1])
+    yr.ft <- as.numeric(pr[,3])
+    
+    fl.bs <- grep(yr.bs, fls.hst, value = T) %>% grep('down', ., value = T) 
+    fl.bs.rw <- grep(yr.bs, fls.hst.raw, value = T) 
+    fl.ft <- grep(yr.ft, fls.ftr, value = T)
+    
+    rs.bs <- rast(fl.bs)
+    plot(rs.bs) # Correr y borrar
+    rs.bs.rw <- rast(fl.bs.rw)
+    rs.ft <- rast(fl.ft)
+    
+    dts.bs <- seq(as.Date(paste0(yr.bs, '-01-01'), format = '%Y-%m-%d'), as.Date(paste0(yr.bs, '-12-31'), format = '%Y-%m-%d'), by = 'day')
+    dts.ft <- seq(as.Date(paste0(yr.ft, '-01-01'), format = '%Y-%m-%d'), as.Date(paste0(yr.ft, '-12-31'), format = '%Y-%m-%d'), by = 'day')
+    
+    if(leap_year(yr.bs)){
+      print('leap year basename')
+      dts.bs <- dts.bs[-grep('02-29', dts.bs, value = F)]
+    }
+    
+    if(leap_year(yr.ft)){
+      print('leap year future')  
+      dts.ft <- dts.ft[-grep('02-29', dts.ft, value = F)]
+    }
+    
+    time(rs.bs) <- dts.bs
+    time(rs.bs.rw) <- dts.bs
+    time(rs.ft) <- dts.ft
+    
+    anml <- map(.x = 1:12, .f = function(m){
+      
+      m <- 1 # Correr y borrar
+      
+      print(month.abb[m])
+      mn <- ifelse(m < 10, paste0('0', m), as.character(m))
+      
+      # To filtering the monts
+      r.bs <- rs.bs[[grep(glue('-{mn}-'), time(rs.bs), value = FALSE)]]
+      r.bs.r <- rs.bs.rw[[grep(glue('-{mn}-'), time(rs.bs.rw), value = FALSE)]]
+      r.ft <- rs.ft[[grep(glue('-{mn}-'), time(rs.ft), value = FALSE)]]
+      
+      r.bs <- sum(r.bs)
+      r.bs.r <- sum(r.bs.r)
+      r.ft <- sum(r.ft)
+      
+      if(nrow(as.data.frame(r.bs)) == 0){
+        cat('NA ')
+        r.bs[] <- 0
+        r.bs <- terra::crop(r.bs, bsin)
+        r.bs <- terra::mask(r.bs, bsin)
+      }  
+      
+      plot(c(r.bs.r, r.ft))
+      
+      r.bs <- r.bs * 86400
+      r.bs.r <- r.bs.r * 86400
+      r.ft <- r.ft * 86400
+      
+      #calculate anomaly in fraction
+      anom <- (r.ft - r.bs.r)/r.bs.r
+      plot(anom)
+      
+      # Truncate the top 2% of anomaly values
+      thr <- as.numeric(terra::global(x = anom, fun = stats::quantile, probs = 0.98, na.rm = T))
+      anom[anom >= thr] <- thr
+      
+      crd <- terra::as.data.frame(anom, xy = T)
+      names(crd)[3] <- 'mean'      
+      crd <- mutate(crd, mean = ifelse(is.inf(mean), 0, mean))
+      
+      library(fields)
+      tps <- fields::Tps(x = crd[,c('x', 'y')], Y = crd[,'mean']) 
+      ref <- r.bs[[1]] * 0 + 1
+      plot(ref)
+      int <- terra::interpolate(object = rast(ref), model = tps, fun = predict)
+      int <- terra::mask(int, mask = ref)
+      int <- terra::crop(int, bsin)
+      int <- terra::mask(int, bsin)
+      cat('Done\n')
+      return(int)
+      
+    })
+    
+  })
   
   
   

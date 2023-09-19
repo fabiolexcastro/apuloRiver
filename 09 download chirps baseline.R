a9253ab6405040d267aaa01874e7e20b43f84e85
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-19 14:57:21 -0500

    update

--- 09 download chirps baseline.R
+++ 09 download chirps baseline.R
@@ -11,6 +11,9 @@ pacman::p_load(elevatr, spatialEco, terra, RSAGA, fs, sf, readxl, openxlsx, tidy
 install.packages('chirps')
 library(chirps)
 
+library(remotes)
+remotes::install_github("mikejohnson51/climateR")
+
 g <- gc(reset = TRUE)
 rm(list = ls())
 options(scipen = 999, warn = -1)

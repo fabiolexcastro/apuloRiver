bf2d3e0f06511ce9dad08ffc0ed1c3899b62844d
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-19 14:54:32 -0500

    update

---
+++ 09 download chirps baseline.R
@@ -0,0 +1,18 @@
+
+
+
+## Extract values from the points
+## September 19 th 2023
+
+# Load libraries ----------------------------------------------------------
+library(pacman)
+pacman::p_load(elevatr, spatialEco, terra, RSAGA, fs, sf, readxl, openxlsx, tidyverse, glue, gtools, RColorBrewer)
+
+install.packages('chirps')
+library(chirps)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+

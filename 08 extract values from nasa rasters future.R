30530591d7e80bfd91ab488a1cba1689bae0aafb
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-20 16:49:03 -0500

    update

--- 08 extract values from nasa rasters future.R
+++ 08 extract values from nasa rasters future.R
@@ -77,6 +77,10 @@ extrac.prec.ftre <- function(ssp, mdl){
     
     dts.raw <- time(rst)
     
+    yea <- prds[i, 3]
+    yea <- as.numeric(yea)
+    sqn <- seq(as.Date(glue('{yea}-01-01'), format = '%Y-%m-%d'), as.Date(glue('{yea}-12-31'), format = '%Y-%m-%d'), by = 'day')
+    
     if(length(sqn) == 365){
       cat('Not leap year')
     } else {
@@ -84,12 +88,9 @@ extrac.prec.ftre <- function(ssp, mdl){
       sqn <- sqn[-grep('-02-29', sqn, value = F)]
     }
     
-    yea <- prds[i, 3]
-    yea <- as.numeric(yea)
-    sqn <- seq(as.Date(glue('{yea}-01-01'), format = '%Y-%m-%d'), as.Date(glue('{yea}-12-31'), format = '%Y-%m-%d'), by = 'day')
-    
     vls <- spread(vls, ID, value)
     vls <- mutate(vls, var = sqn)
+    vls <- gather(vls, variable, value, -var)
     vls <- mutate(vls, value = ifelse(is.na(value), 0, value))
     vls <- mutate(vls, model = basename(mdl), date = var)
     vls <- mutate(vls, var = 'prec')

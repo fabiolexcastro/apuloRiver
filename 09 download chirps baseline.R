46520dab6af6b31f428d8e7c9b9bfdb1738a86db
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-22 09:48:13 -0500

    update

--- 09 download chirps baseline.R
+++ 09 download chirps baseline.R
@@ -17,8 +17,9 @@ options(scipen = 999, warn = -1)
 # Load data ---------------------------------------------------------------
 bsin <- terra::vect('./shp/Cuenca/Cuenca_Río_Apulo.shp')
 plot(bsin)
+bsin <- terra::vect('../shp/Cuenca/Cuenca_Río_Apulo.shp')
 
-
+chrp <- climateR::getCHIRPS(AOI = st_as_sf(bsin), startDate = '1983-01-01', endDate = '2014-12-31')
 
 
 

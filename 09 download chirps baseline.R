852516160ba8b82eeb378176fe4fb5872b77aa3c
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-22 09:44:36 -0500

    update

--- 09 download chirps baseline.R
+++ 09 download chirps baseline.R
@@ -8,13 +8,16 @@
 library(pacman)
 pacman::p_load(elevatr, remotes, chirps, spatialEco, terra, RSAGA, fs, sf, readxl, openxlsx, tidyverse, glue, gtools, RColorBrewer)
 
-remotes::install_github("mikejohnson51/climateR")
+# remotes::install_github("mikejohnson51/climateR")
 
 g <- gc(reset = TRUE)
 rm(list = ls())
 options(scipen = 999, warn = -1)
 
 # Load data ---------------------------------------------------------------
+bsin <- terra::vect('./shp/Cuenca/Cuenca_Río_Apulo.shp')
+plot(bsin)
+
 
 
 

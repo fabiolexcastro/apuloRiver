398b3e09d0941953ecb1b6f6090212ce87c93334
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-22 10:07:39 -0500

    Update

--- 09 download chirps baseline.R
+++ 09 download chirps baseline.R
@@ -38,7 +38,7 @@ nlyr(chrp)
 
 # Temperature -------------------------------------------------------------
 dtes <- seq(as.Date('1983-01-01', format = '%Y-%m-%d'), as.Date('2014-12-31', format = '%Y-%m-%d'), by = 'day')
-chrx <- get_chirts(object = chrp, dates = dtes, var = 'Tmax', as.raster = TRUE)
+chrx <- get_chirts(object = bsin, dates = dtes, var = 'Tmax', as.raster = TRUE)
 
 
 #

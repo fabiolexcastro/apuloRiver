8d3be09e96a91504f5b8e136fabbfa52a3cdeba5
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-27 11:32:20 -0500

    check chirps data downloaded

---
+++ 10 check chirps.R
@@ -0,0 +1,19 @@
+
+
+# Check chirps dataset
+
+# Load libraries
+require(pacman)
+pacman::p_load(terra, fs, sf, tidyverse, rgeos, gtools, stringr, lubridate)
+
+g <- gc(reset = T)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+
+# Load data ---------------------------------------------------------------
+path <- './tif/chirps/raw/chirps_apulo-bsin.tif'
+rstr <- rast(path)
+
+rstr
+plot(rstr)

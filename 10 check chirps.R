aba4d038c187a5a98ad29b775c558b82a3652942
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-27 11:57:36 -0500

    update

--- 10 check chirps.R
+++ 10 check chirps.R
@@ -34,8 +34,9 @@ year <- unique(year)
 # Function ----------------------------------------------------------------
 extract.values <- function(yr){
   
-  yr <- year[1]
+  # yr <- year[1]
   
+  cat('To process: ', yr, '\n')
   rst <- rstr[[grep(paste0(yr, '-'), names(rstr), value = FALSE)]]
   vls <- terra::extract(rst, pnts[,1:2])
   vls <- cbind(pnts, vls)
@@ -43,9 +44,15 @@ extract.values <- function(yr){
   vls <- gather(vls, var, value, -Long_, -Lat, -Subbasin, -ID)
   vls <- dplyr::select(vls, -ID, Subbasin, Lon = Long_, Lat = Lat, var, value)
   vls <- mutate(vls, date = str_sub(var, 8, nchar(var)))
-  
-  
+  vls <- mutate(vls, date = as.Date(date, format = '%Y-%m-%d'))
+  vls <- dplyr::select(vls, -var)
+  cat('Done!\n')
+  return(vls)
   
 }
+
+# To apply the function ---------------------------------------------------
+tbls <- map(.x = year, .f = extract.values)
+
 
 

3ed836e4d4b1073a68649019071e95c913b7028a
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-29 16:39:57 -0500

    update

--- 11 extract values for chirps - chirts.R
+++ 11 extract values for chirps - chirts.R
@@ -20,10 +20,11 @@ head(pnts)
 
 pnts <- pnts %>% dplyr::select(Subbasin, Long_, Lat)
 
+# Functions to use --------------------------------------------------------
 extr.vles <- function(yr, vr){
   
-  yr <- 1983 # Correr y borrar
-  vr <- 'Tmin'
+  # yr <- 1983 # Correr y borrar
+  # vr <- 'Tmin'
   
   cat('To process: ', yr, '\n')
   fls <- grep(yr, fles, value = T)
@@ -45,3 +46,9 @@ extr.vles <- function(yr, vr){
   return(vls)
   
 }
+
+# To extract the values  --------------------------------------------------
+tmin.vles <- map(.x = 1:length(year), .f = function(i){
+  extr.vles(yr = year[i], var = 'Tmin')
+})
+

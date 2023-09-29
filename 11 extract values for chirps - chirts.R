791a3053d34f2c304ebc76b36ea0188eaf79f8ad
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-09-29 16:21:14 -0500

    update

--- 11 extract values for chirps - chirts.R
+++ 11 extract values for chirps - chirts.R
@@ -20,14 +20,16 @@ head(pnts)
 
 pnts <- pnts %>% dplyr::select(Subbasin, Long_, Lat)
 
-extr.vles <- function(yr){
+extr.vles <- function(yr, vr){
   
   yr <- 1983 # Correr y borrar
+  vr <- 'Tmin'
   
   cat('To process: ', yr, '\n')
   fls <- grep(yr, fles, value = T)
+  fls <- grep(vr, fls, value = T)
   fls <- as.character(fls)
   rst <- rast(fls)
-  plot(rst)
+  vls <- terra::extract(v)
   
 }

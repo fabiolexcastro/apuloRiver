c1ce462b8af27e9ac9b32432590829569b65a813
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-02 15:28:04 -0500

    update

--- 12 comparison chirps vs gcms.R
+++ 12 comparison chirps vs gcms.R
@@ -2,7 +2,7 @@
 
 # Load libraries ----------------------------------------------------------
 library(pacman)
-pacman::p_load(elevatr, remotes, chirps, spatialEco, terra, RSAGA, fs, sf, readxl, openxlsx, tidyverse, glue, gtools, RColorBrewer)
+pacman::p_load(elevatr, remotes, readxl, xlsx, openxlsx, chirps, spatialEco, terra, RSAGA, fs, sf, readxl, openxlsx, tidyverse, glue, gtools, RColorBrewer)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
@@ -17,3 +17,9 @@ fles <- as.character(fles)
 fles.prec <- grep('prec', fles, value = T)
 prec.bsln <- grep('prec-hist', fles.prec, value = T)
 prec.ftre <- grep('prec-ftre', fles.prec, value = T)
+
+# Baseline 
+prec.bsln <- map(.x = prec.bsln, .f = read.xlsx)
+
+# Future 
+prec.ftre <- map(.x = prec.ftre, .f = read.xlsx)

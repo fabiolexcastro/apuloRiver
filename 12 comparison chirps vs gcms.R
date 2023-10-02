b665eb1bd4a9676bcfbc782b9946cf80b3f921c4
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-02 15:16:44 -0500

    update

---
+++ 12 comparison chirps vs gcms.R
@@ -0,0 +1,15 @@
+
+
+# Load libraries ----------------------------------------------------------
+library(pacman)
+pacman::p_load(elevatr, remotes, chirps, spatialEco, terra, RSAGA, fs, sf, readxl, openxlsx, tidyverse, glue, gtools, RColorBrewer)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+path <- './data/tbl'
+fles <- dir_ls(path, regexp = '.csv$')
+
+

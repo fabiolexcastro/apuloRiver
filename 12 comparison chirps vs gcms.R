f802b5f57af30ab3f621295d82272ae33ab4181a
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-02 15:46:24 -0500

    update

--- 12 comparison chirps vs gcms.R
+++ 12 comparison chirps vs gcms.R
@@ -37,7 +37,7 @@ prec.bsln <- bind_rows(prec.bsln)
 write.xlsx(prec.bsln, './data/tbl/values-sts_GCMs_prec-hist.xlsx')
 
 prec.chrp <- grep('values_chirps-bsln_raw.csv', fles, value = T)
-prec.chrp <- read.xlsx(prec.chrp)
+prec.chrp <- read_csv(prec.chrp)
 head(prec.chrp)
 
 

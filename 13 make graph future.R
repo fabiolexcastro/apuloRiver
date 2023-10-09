52ef6c46f24cd794e008fe85b6fea0742152b85b
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-09 17:43:46 -0500

    'Update'

--- 13 make graph future.R
+++ 13 make graph future.R
@@ -19,56 +19,17 @@ pnts <- read.xlsx('./data/tbl/Subcuencas Coordenadas.xlsx')
 pnts <- dplyr::select(pnts, Long_, Lat, Subbasin)
 
 # Function to use ---------------------------------------------------------
-extr.vles <- function(sspe, mdel){
+extr.vles <- function(sspe, mdel, varb){
   
   sspe <- 'ssp245'
   mdel <- mdls[1]
+  varb <- 'pr'
   
-  fles <- grep(sspe, dir_ls(path), value = TRUE) %>% 
-    dir_ls() %>% 
-    as.character() %>% 
-    grep(mdel, ., value = T) %>% 
-    dir_ls() %>% 
-    as.character() %>% 
-    map(., dir_ls) %>% 
-    unlist() %>% 
-    as.character() %>% 
-    grep('down', ., value = T) %>% 
-    map(., dir_ls) %>% 
-    unlist() %>% 
-    as.character() %>% 
-    grep('.tif$', ., value = T)
+  fles <- dir_ls('./data/tbl', regexp = '.xlsx$')
+  fles <- grep(varb, fles, value = T)
   
-  map(.x = 1:3, .f = function(v){
-    
-    v <- 1 # Correr y borrar
-    cat('To process:', vars[v], '\n')
-    fls <- grep(vars[v], fles, value = T)
-    var <- vars[v]
-    
-    map(.x = 1:length(fls), .f = function(r){
-      
-      r <- 1 # Correr y borrar 
-      cat('Raster: ', r, '\n')
-      r <- fls[r]
-      r <- rast(r)
-      z <- terra::extract(r, pnts[,c('Long_', 'Lat')])
-      z <- cbind(pnts, z)
-      z <- as_tibble(z)
-      z <- gather(z, var, value, -Long_, -Lat, -Subbasin, -ID)
-      z <- mutate(z, date = as.Date(var, format = '%Y-%m-%d'))
-      z <- z[,-5]
-      
-      if(vars[v] == 'pr'){
-        cat('Prec\t')
-        z <- mutate(z, value = ifelse(value < 0, 0, value))
-      } 
-      
-    })
-    
-    
-    
-  })
+  
+
   
   
   

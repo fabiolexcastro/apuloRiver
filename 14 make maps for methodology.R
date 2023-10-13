76028990552063a2fb49f636c5b934ceca242ea1
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-13 16:58:24 -0500

    update

--- 14 make maps for methodology.R
+++ 14 make maps for methodology.R
@@ -18,7 +18,7 @@ rraw.bsl <- rast('./tif/nasa/cmip6/historical/ACCESS-CM2/tasmax/cund_tasmax_day_
 rraw.ftr <- rast('./data/tif/nasa/cmip6/ssp245/ACCESS-CM2/tasmax/cund_tasmax_day_ACCESS-CM2_ssp245_r1i1p1f1_gn_2015.nc')
 
 rgwr.bsl <- rast('./tif/nasa/cmip6/historical/ACCESS-CM2/tasmax/down-cund_tasmax_day_ACCESS-CM2_historical_r1i1p1f1_gn_1974.nc')
-rdwn.ftr <- rast('./tif/nasa/cmip6/ssp245/ACCESS-CM2/tasmax/down/tasmax_2015.tif')
+rdwn.ftr <- rast('./data/tif/nasa/cmip6/ssp245/ACCESS-CM2/tasmax/down/tasmax_2015.tif')
 
 
 

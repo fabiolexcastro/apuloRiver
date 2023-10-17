d0bc2cdd07a21660f03eaffd27ddab40ca13beca
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-17 16:17:53 -0500

    'Update'

--- 14 make maps for methodology.R
+++ 14 make maps for methodology.R
@@ -103,9 +103,36 @@ gsrtm
 dir_create('./png/maps')
 ggsave(plot = gsrtm, filename = './png/maps/srtm_cundinamarca.png', units = 'in', width = 9, height = 7, dpi = 300)
   
+# Temperature raw baseline ------------------------------------------------
 
+graw.bsl <- ggmap(ggbx, alpha = 0.5) + 
+  geom_tile(data = rraw.bsl.tbl.avg, aes(x = x, y = y, fill = value)) + 
+  scale_fill_gradientn(colors = brewer.pal(n = 9, name = 'YlOrRd')) +
+  geom_sf(data = st_as_sf(bsin), fill = NA, col = 'red', inherit.aes = FALSE) + 
+  geom_sf(data = st_as_sf(cndn), fill = NA, col = 'grey30', inherit.aes = FALSE) +
+  coord_sf(xlim = c(-74.88, -73.04), ylim = c(3.73, 5.83)) +
+  labs(x = 'Lon', y = 'Lat', fill = 'Temperatura (°C)') +
+  theme_minimal() +
+  theme(legend.position = 'bottom', 
+        legend.key.width = unit(2.5, 'line'),
+        axis.text.y = element_text(angle = 90, hjust = 0.5),
+        axis.text.x = element_text(hjust = 0.5),
+        text = element_text(family = 'Gill Sans MT')) +
+  guides(fill = guide_legend( 
+    direction = 'horizontal',
+    keyheight = unit(1.15, units = "mm"),
+    keywidth = unit(15, units = "mm"),
+    title.position = 'top',
+    title.hjust = 0.5,
+    label.hjust = .5,
+    nrow = 1,
+    byrow = T,
+    reverse = F,
+    label.position = "bottom"
+  )) 
 
-
+graw.bsl
+ggsave(plot = gsrtm, filename = './png/maps/temp_raw-bsl_cundinamarca.png', units = 'in', width = 9, height = 7, dpi = 300)
 
 
 

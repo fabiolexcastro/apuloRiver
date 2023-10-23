05b1bddb775cb43745fb418fe7ae5be29edc334a
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-23 14:45:14 -0500

    update

---
+++ 16 extract values from the baseline.R
@@ -0,0 +1,24 @@
+
+
+
+# Load libraries ----------------------------------------------------------
+library(pacman)
+pacman::p_load(elevatr, remotes, readxl, xlsx, openxlsx, chirps, spatialEco, terra, RSAGA, fs, sf, readxl, openxlsx, tidyverse, glue, gtools, RColorBrewer)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+
+path <- './data/tif/chirts/bsl/raw'
+fles <- dir_ls(path, regexp = '.tif$')
+vars <- c('Tmax', 'Tmin')
+
+# Coordinates
+bsin <- terra::vect('./shp/Cuenca/Cuenca_Río_Apulo.shp')
+
+# Basin shapefile
+pnts <- read.xlsx('./data/tbl/Subcuencas Coordenadas.xlsx')
+
+

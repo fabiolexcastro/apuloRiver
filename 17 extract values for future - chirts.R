a32b66e2c4376a062f03177e16d794a89e880dfb
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-25 10:13:16 -0500

    'update'

--- 17 extract values for future - chirts.R
+++ 17 extract values for future - chirts.R
@@ -11,3 +11,6 @@ options(scipen = 999, warn = -1)
 # List the files ----------------------------------------------------------
 fles <- dir_ls('./data/tbl') %>% as.character()
 fles.tasm.ftre <- grep('tas', fles, value = T) %>% grep('ftre', ., value = T)
+
+fles.tmin.ftre <- grep('tasmin', fles.tasm.ftre, value = T)
+fles.tmax.ftre <- grep('tasmax', fles.tasm.ftre, value = T)

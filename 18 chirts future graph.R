b3d1b81965e1babc99d2cc9aec4b11046f521006
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-26 09:45:26 -0500

    updatge

--- 18 chirts future graph.R
+++ 18 chirts future graph.R
@@ -18,6 +18,34 @@ fles <- grep('ftre', fles, value = T)
 tmin <- grep('Tmin', fles, value = T) %>% read_csv()
 tmax <- grep('Tmax', fles, value = T) %>% read_csv()
 
+tmin <- read_csv('Tmin_stts_ftre.csv')
+
+
+# Function ----------------------------------------------------------------
+make.graph <- function(tble){
+  
+  tble <- tmin
+  
+  smmr <- tble %>% group_by(variable, model, station, year) %>% dplyr::summarise(value = mean(value)) %>% ungroup()
+  smmr %>% filter(model == 'ACCESS-CM2') %>% filter(station == 1) %>% View()
+  
+  # Start the analysis
+  glne <- ggplot(data = tble, aes(x = year, y = value, col = model)) + 
+    geom_line() + 
+    facet_wrap(.~station) + 
+    theme_minimal() +
+    labs(x = 'Año', y = 'Temperatura (°C)', col = '') +
+    theme(legend.position = 'bottom', 
+          legend.key.width = unit(3, 'line'), 
+          strip.text = element_text(face = 'bold'))
+  
+  glne
+  
+  
+  
+}
+
+
 
 
 

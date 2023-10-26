a72d9d7d1b95a697441cad4bc3d0ac7941109c41
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-26 09:37:16 -0500

    udpate

--- 18 chirts future graph.R
+++ 18 chirts future graph.R
@@ -13,4 +13,13 @@ path <- './data/tbl/values_stts_tasm'
 fles <- dir_ls(path)
 fles <- as.character(fles)
 fles <- grep('ftre', fles, value = T)
-fles
+
+# Tidy the table ----------------------------------------------------------
+tmin <- grep('Tmin', fles, value = T) %>% read_csv()
+head(tmin)
+
+
+
+
+
+

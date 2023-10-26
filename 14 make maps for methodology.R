54f66780ab3348b643861f1ca3dd8a104874b192
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-26 10:57:10 -0500

    update

--- 14 make maps for methodology.R
+++ 14 make maps for methodology.R
@@ -282,6 +282,8 @@ ggsave(plot = ginterpolate, filename = './png/maps/temp_int-cnt_cundinamarca_v2.
 # Downscaled figure -------------------------------------------------------
 rdwn.ftr.tbl.avg
 
+bsin.bffr <- terra::buffer(bsin.bffr, 0.2)
+
 gdown <- autoplot(map.latlon) + 
   geom_tile(data = rdwn.ftr.tbl.avg, aes(x = x, y = y, fill = value)) + 
   scale_fill_gradientn(colors = brewer.pal(n = 9, name = 'YlOrRd')) +

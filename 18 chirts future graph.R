ec6796149bff64052ee2d308f4846b245f984be0
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-26 09:34:34 -0500

    update

---
+++ 18 chirts future graph.R
@@ -0,0 +1,14 @@
+
+
+# Load libraries ----------------------------------------------------------
+library(pacman)
+pacman::p_load(elevatr, remotes, readxl, xlsx, openxlsx, chirps, spatialEco, terra, RSAGA, fs, sf, readxl, openxlsx, tidyverse, glue, gtools, RColorBrewer)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+path <- './data/tbl/values_stts_tasm'
+fles <- dir_ls(path)
+fles <- as.character(fles)

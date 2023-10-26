f8bee66d4ef296919d8480830f14497acf82948b
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-10-26 09:58:00 -0500

    'udate'

--- 18 chirts future graph.R
+++ 18 chirts future graph.R
@@ -59,11 +59,53 @@ make.graph <- function(tble){
           legend.key.width = unit(3, 'line'), 
           strip.text = element_text(face = 'bold'))
   
-  ggsave(plot = glne_245, filename = '../png/tasm/tmin_ftre_ssp245.png', units = 'in', width = 12, height = 9, dpi = 300)
-  ggsave(plot = glne_585, filename = '../png/tasm/tmin_ftre_ssp585.png', units = 'in', width = 12, height = 9, dpi = 300)
+  ggsave(plot = glne_245, filename = './png/tasm/tmin_ftre_ssp245.png', units = 'in', width = 12, height = 9, dpi = 300)
+  ggsave(plot = glne_585, filename = './png/tasm/tmin_ftre_ssp585.png', units = 'in', width = 12, height = 9, dpi = 300)
   cat('Done\n')
   
 }
+
+
+# Tmax --------------------------------------------------------------------
+tble <- tmax
+mdls <- unique(tble$model)
+
+tble <- map_dfr(.x = 1:5, .f = function(i){
+  tbl <- tble %>% filter(model == mdls[i])
+  tbl <- map_dfr(.x = 1:4, .f = function(j){
+    tbl <- tbl %>% filter(station == j)
+    tbl <- mutate(tbl, ssp = c(rep('ssp245', 85), rep('ssp585', 85)))
+  })
+  return(tbl)
+})
+
+# Start the analysis
+glne_245 <- ggplot(data = tble %>% filter(ssp == 'ssp245'), aes(x = year, y = value, col = model)) + 
+  geom_line() + 
+  facet_wrap(.~station) + 
+  ggtitle(label = 'SSP: 245') +
+  theme_minimal() +
+  labs(x = 'Año', y = 'Temperatura (°C)', col = '') +
+  theme(legend.position = 'bottom', 
+        plot.title = element_text(face = 'bold', hjust = 0.5),
+        legend.key.width = unit(3, 'line'), 
+        strip.text = element_text(face = 'bold'))
+
+glne_585 <- ggplot(data = tble %>% filter(ssp == 'ssp585'), aes(x = year, y = value, col = model)) + 
+  geom_line() + 
+  facet_wrap(.~station) + 
+  ggtitle(label = 'SSP: 585') +
+  theme_minimal() +
+  labs(x = 'Año', y = 'Temperatura (°C)', col = '') +
+  theme(legend.position = 'bottom', 
+        plot.title = element_text(face = 'bold', hjust = 0.5),
+        legend.key.width = unit(3, 'line'), 
+        strip.text = element_text(face = 'bold'))
+
+ggsave(plot = glne_245, filename = './png/tasm/tmax_ftre_ssp245.png', units = 'in', width = 12, height = 9, dpi = 300)
+ggsave(plot = glne_585, filename = './png/tasm/tmax_ftre_ssp585.png', units = 'in', width = 12, height = 9, dpi = 300)
+cat('Done\n')
+
 
 
 
